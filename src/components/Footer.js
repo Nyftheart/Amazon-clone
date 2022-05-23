90cb764835831993c5f1259a4740f29a8b4a7cf1
Nyftheart <nyftheart@MacNyft.local>
2022-05-23 15:04:30 +0200

    add page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,36 @@
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+
 function Footer() {
     return (
         <div className="flex flex-col">
+            <div id="btn-back-to-top" className="text-sm p-3 w-full flex justify-center bg-amazon_blue-light">
+                <p className="link text-gray-300">Back To top</p>
+            </div>
             <div className="max-w-full flex flex-col md:flex-row md:justify-around bg-amazon_blue text-white h-full pb-5">
                 <div className="ml-5">
                     <h3 className="mt-4">Nous Connaitre</h3>
