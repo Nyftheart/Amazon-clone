7839994261990bb8a073b1439c755dd9ff5eda88
Nyftheart <nyftheart@MacNyft.local>
2022-05-23 15:17:52 +0200

    add page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,49 +1,22 @@
-let mybutton = document.getElementById("btn-back-to-top");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-    scrollFunction();
-};
-
-function scrollFunction() {
-    if (
-        document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20
-    ) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
-
-
 function Footer() {
     return (
         <div className="flex flex-col">
-            <div id="btn-back-to-top" className="text-sm p-3 w-full flex justify-center bg-amazon_blue-light">
+            <div className="text-sm p-3 w-full flex justify-center bg-amazon_blue-light">
                 <p className="link text-gray-300">Back To top</p>
             </div>
             <div className="max-w-full flex flex-col md:flex-row md:justify-around bg-amazon_blue text-white h-full pb-5">
                 <div className="ml-5">
-                    <h3 className="mt-4">Nous Connaitre</h3>
+                    <h3 className="mt-4">Get to Know Us</h3>
                     <div className="text-sm leading-snug mt-2 text-gray-300">
-                        <p className="link">A Propos de nous</p>
-                        <p className="link">Carierre</p>
-                        <p className="link">Nouvelle Amelioration</p>
-                        <p className="link">Amazon Protection</p>
-                        <p className="link">Donnez Votre Avis</p>
+                        <p className="link">About Us</p>
+                        <p className="link">Careers</p>
+                        <p className="link">Press Releases</p>
+                        <p className="link">Amazon Cares</p>
+                        <p className="link">Gift a Smile</p>
                     </div>
                 </div>
                 <div className="ml-5">
-                    <h3 className="mt-4">Contact</h3>
+                    <h3 className="mt-4">Contact With Us</h3>
                     <div className="text-sm leading-snug mt-2 text-gray-300">
                         <p className="link">Facebook</p>
                         <p className="link">Twitter</p>
@@ -51,30 +24,33 @@ function Footer() {
                     </div>
                 </div>
                 <div className="ml-5">
-                    <h3 className="mt-4">Proposer vos produit</h3>
+                    <h3 className="mt-4">Make Money With Us</h3>
                     <div className="text-sm leading-snug mt-2 text-gray-300">
-                        <p className="link">Vendre sur Amazon</p>
-                        <p className="link">Vendre sous le nom d'Amazon </p>
-                        <p className="link">Vendre sur Amazon Global</p>
-                        <p className="link">Etre Affilier</p>
+                        <p className="link">Sell On Amazon</p>
+                        <p className="link">Sell under Amazon Acceleration</p>
+                        <p className="link">Amazon Global Selling</p>
+                        <p className="link">Become an Affiliate</p>
+                        <p className="link">Fulfilment By Amazon</p>
+                        <p className="link">Advertise Your Product</p>
+                        <p className="link">Amazon Pay on Merchants</p>
                     </div>
                 </div>
                 <div className="ml-5">
-                    <h3 className="mt-4">On vous Aide</h3>
+                    <h3 className="mt-4">Let us Help You</h3>
                     <div className="text-sm leading-snug mt-2 text-gray-300">
-                        <p className="link">COVID-19 & Amazon</p>
-                        <p className="link">Votre Compte</p>
-                        <p className="link">Retour Article</p>
-                        <p className="link">Achat 100% Protéger</p>
-                        <p className="link">Amazon App</p>
-                        <p className="link">Amazon Assistant</p>
-                        <p className="link">Aide</p>
+                        <p className="link">COVID-19 and Amazon</p>
+                        <p className="link">Your Account</p>
+                        <p className="link">Return Center</p>
+                        <p className="link">100% Purchase Protection</p>
+                        <p className="link">Amazon App Download</p>
+                        <p className="link">Amazon Assistant Download</p>
+                        <p className="link">Help</p>
                     </div>
                 </div>
             </div>
             <div className="flex text-sm p-3 space-x-3 w-full justify-center bg-amazon_blue">
                 <p className="flex text-gray-300">Made By </p>
-                <h1 className="link text-gray-300 font-bold">NyftHeart</h1>
+                <h1 className="link text-gray-300 font-bold">Aman Janwani</h1>
             </div>
         </div>
     )
